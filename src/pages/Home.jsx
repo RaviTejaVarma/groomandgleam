227e9516413abc4efddf4d4d08c63b5331366f37
Asrithdatla369 <asrithvarma947@gmail.com>
2024-07-03 13:55:31 +0530

    file correction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Hero } from "../sections/Hero";
+import Aboutus from "../sections/Aboutus";
 
 const Home = () => {
   return <div>
     <Hero/>
+    <Aboutus/>
   </div>;
 };
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './index.css';
+import '../App.css';
 
 export const Hero = () => {
     return (
